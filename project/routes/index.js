0af6334acf74ae6c3d87373e2a22ab55819aecac
Administrator <lionel@buzzy.cc>
2018-12-23 21:46:07 +0800

    Ok

--- project/routes/index.js
+++ project/routes/index.js
@@ -4,7 +4,7 @@ var NovelTransferModel = require('../model/NovelTransfer');
 router.get('/:randomID', async (ctx, next) => {
 	let novel= await NovelTransferModel.findOne({randomID:ctx.params.randomID})
 	if(novel){
-		let url = 'http://erji.nyzda.top/novel_transfer/' + novel._id;
+		let url = 'http://erji.rrdtjj.top/novel_transfer/' + novel._id;
 		ctx.response.redirect(url);
 	}else{
 		ctx.body= 'no url translate'
